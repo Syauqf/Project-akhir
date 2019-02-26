0b582a5ef8b69594efcfa6a4c6f112fcb3cffccd
syauqii <ahmad_syauqi_26rpl@student.smktelkom-mlg.sch.id>
2019-02-26 14:38:59 +0700

    Project akhir 26/02/2019

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('testreact');
+});
+
+Route::get('/login_page','UsersController@index');
+Route::post('/register_user','UsersController@register');
+Route::post('/user_login','UsersController@login');
+Route::get('/logout','UsersController@logout');
+
+Route::group(['middleware'=>'FrontLogin_middleware'],function (){
+    Route::get('/myaccount','UsersController@account');
+    Route::put('/update-profile/{id}','UsersController@updateprofile');
+    Route::put('/update-password/{id}','UsersController@updatepassword');
+    Route::get('/check-out','CheckOutController@index');
+    Route::post('/submit-checkout','CheckOutController@submitcheckout');
+    Route::get('/order-review','OrdersController@index');
+    Route::post('/submit-order','OrdersController@order');
+    Route::get('/cod','OrdersController@cod');
+    Route::get('/paypal','OrdersController@paypal');
+});
